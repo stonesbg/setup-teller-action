106fd11bcad3b97ef999341a2016fc5fbfbc95f2
Brian Garvey <brian.garvey@alvaria.com>
2025-01-21 11:46:21 +0000

    fix up file extension

--- src/install.js
+++ src/install.js
@@ -61,7 +61,7 @@ function zipName(version) {
   let zip_name = `teller_${version}_${platform}_${arch}.tar.gz`;
 
   if (version.startsWith("2")) {
-    zip_name = `teller-${arch}-${platform.toLowerCase()}.tar.gz`;
+    zip_name = `teller-${arch}-${platform.toLowerCase()}.tar.xz`;
   }
 
   return zip_name;
