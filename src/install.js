6cfb4aae2dda6ec67dace08f6a1e97b83b1709b7
Brian Garvey <brian.garvey@alvaria.com>
2025-01-21 11:44:15 +0000

    fix up v2 naming for zip

--- src/install.js
+++ src/install.js
@@ -61,7 +61,7 @@ function zipName(version) {
   let zip_name = `teller_${version}_${platform}_${arch}.tar.gz`;
 
   if (version.startsWith("2")) {
-    zip_name = `teller_${platform}_${arch}.tar.gz`;
+    zip_name = `teller-${arch}-${platform.toLowerCase()}.tar.gz`;
   }
 
   return zip_name;
